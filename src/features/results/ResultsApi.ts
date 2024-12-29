8abd10905583c66b2cb9507ed3df6546e24e6cd1
CedricSanchezGitHub <mycedmail@gmail.com>
2024-12-29 16:10:33 +0100

    [add] Ajout du mock pour dev. taff sur results/index.astro

--- src/features/results/ResultsApi.ts
+++ src/features/results/ResultsApi.ts
@@ -1,6 +1,7 @@
 import type {ResultsEntity} from './ResultsEntity';
 
-const URL_BACKEND = 'http://localhost:8081/getresults/getmatchsresults';
+// const URL_BACKEND = 'http://localhost:8081/getresults/getmatchsresults';
+const URL_BACKEND = '/resultsMock.json'
 
 /**
  * Récupère la liste des résultats depuis l'API
@@ -10,14 +11,24 @@ const URL_BACKEND = 'http://localhost:8081/getresults/getmatchsresults';
 export async function fetchResults(): Promise<ResultsEntity[]> {
     try {
         const response = await fetch(URL_BACKEND);
-
         if (!response.ok) {
-            console.error(`Erreur lors de la récupération des résultats : ${response.statusText}`);
-            return [];
+            throw new Error(`Erreur lors de la récupération des résultats : ${response.statusText}`);
         }
+        const data = await response.json();
 
-        return await response.json();
+        console.log(data);
 
+        return data.map((item: any) => ({
+            id: item.id || 0,
+            date: item.date || "Données manquantes",
+            team1Name: item.team1Name || "Données manquantes",
+            team1Score: item.team1Score ?? null,
+            team2Name: item.team2Name || "Données manquantes",
+            team2Score: item.team2Score ?? null,
+            matchLink: item.matchLink || null,
+            competition: item.competition || "Données manquantes",
+            day: item.day || "Données manquantes",
+        }));
     } catch (error) {
         console.error('Erreur dans fetchResults:', error);
         return [];
